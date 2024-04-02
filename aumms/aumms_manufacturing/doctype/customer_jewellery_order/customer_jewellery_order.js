2708df1270ba668397999b0d1615dd0209696b89
Keerthana-ck <keerthana98323@gmail.com>
2024-04-02 15:07:32 +0530

    feat : Calcualte Amount in Customer Jewellery Order

--- aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.js
+++ aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.js
@@ -2,15 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Customer Jewellery Order", {
-  customer_expected_total_weight: function (frm) {
-    calculate_customer_expected_amount(frm);
-  },
-  uom: function (frm) {
-    calculate_customer_expected_amount(frm);
-  },
-  purity: function (frm) {
-    calculate_customer_expected_amount(frm);
-  },
   refresh: function(frm){
     frm.set_query('uom',() => {
  			 return {
@@ -18,44 +9,93 @@ frappe.ui.form.on("Customer Jewellery Order", {
  							 "is_purity_uom": 1
  					 }
  			 }
- 	 });
-   if(!frm.is_new){
-     frm.set_df_property('status', 'readonly', 0)
-   }
-   else{
-     frm.set_df_property('status', 'readonly', 1)
-   }
+ 	 })
   }
 });
 
 frappe.ui.form.on("Customer Jewellery Order Details", {
+  purity: function(frm, cdt, cdn){
+    get_board_rate(frm, cdt, cdn)
+  },
+  item_type: function(frm, cdt, cdn){
+    get_board_rate(frm, cdt, cdn)
+  },
+  stock_uom: function(frm, cdt, cdn){
+    get_board_rate(frm, cdt, cdn)
+  },
+  qty: function (frm, cdt, cdn) {
+    let d = locals[cdt][cdn];
+    calculate_amount(frm, cdt, cdn);
+  },
+  board_rate: function (frm, cdt, cdn) {
+    let d = locals[cdt][cdn];
+    calculate_amount(frm, cdt, cdn);
+  },
   expected_weight_per_quantity: function (frm, cdt, cdn) {
     let d = locals[cdt][cdn];
-    var total_weightage = 0;
-    frm.doc.order_item.forEach(function (d) {
-      total_weightage += d.expected_weight_per_quantity * d.item_quantity;
-    });
-    frm.set_value("total_expected_weight_per_quantity", total_weightage);
+    calculate_amount(frm, cdt, cdn);
+    calculate_totals(frm);
   },
-  item_quantity: function (frm, cdt, cdn) {
+  making_chargein_percentage:function (frm, cdt, cdn){
     let d = locals[cdt][cdn];
-    var total_weightage = 0;
-    frm.doc.order_item.forEach(function (d) {
-      total_weightage += d.expected_weight_per_quantity * d.item_quantity;
-    });
-    frm.set_value("total_expected_weight_per_quantity", total_weightage);
+    calculate_amount(frm, cdt, cdn);
+    calculate_totals(frm);
   },
-  order_item_remove: function (frm) {
-    var total_weightage = 0;
-    frm.doc.order_item.forEach(function (d) {
-      total_weightage += d.expected_weight_per_quantity;
-    });
-    frm.set_value("total_expected_weight_per_quantity", total_weightage);
-  },
+  order_item_remove: function (frm, cdt, cdn) {
+    calculate_totals(frm)
+  }
 });
 
-function calculate_customer_expected_amount(frm) {
-  if (frm.doc.customer_expected_total_weight && frm.doc.uom && frm.doc.purity) {
-    board_rate = frm.call("calculate_customer_expected_amount");
+function get_board_rate(frm, cdt, cdn){
+  var d=locals[cdt][cdn];
+  if(d.item_type && d.purity && d.stock_uom){
+    frappe.call({
+      method: 'aumms.aumms.utils.get_board_rate',
+      args: {
+          'item_type': d.item_type,
+          'date': frappe.datetime.get_today(),
+          'purity': d.purity,
+          'stock_uom': d.stock_uom
+      },
+      callback: function(r) {
+        if (r.message) {
+          let board_rate = r.message
+          console.log(board_rate);
+          frappe.model.set_value(cdt, cdn, 'board_rate', board_rate);
+          frm.refresh_field('order_item');
+        }
+      }
+    });
   }
 }
+
+function calculate_amount(frm, cdt, cdn){
+  var child=locals[cdt][cdn];
+  if(child.qty && child.expected_weight_per_quantity && child.board_rate){
+    frappe.model.set_value(cdt, cdn, 'amount_without_making_charge', child.qty * child.expected_weight_per_quantity * child.board_rate);
+  }
+  else {
+    frappe.model.set_value(cdt, cdn, 'amount_without_making_charge', 0);
+  }
+  if(child.making_chargein_percentage && child.amount_without_making_charge){
+    frappe.model.set_value(cdt, cdn, 'amount', child.amount_without_making_charge * (child.making_chargein_percentage/100));
+  }
+  else{
+    frappe.model.set_value(cdt, cdn, 'amount', 0);
+  }
+  frm.refresh_field('order_item');
+}
+
+function calculate_totals(frm , cdt, cdn) {
+  var total_weightage = 0;
+  var total_amount = 0;
+  var total_making_charge = 0
+  frm.doc.order_item.forEach(function (d) {
+    total_weightage += d.expected_weight_per_quantity;
+    total_amount += d.amount;
+    total_making_charge += d.making_chargein_percentage;
+  });
+  frm.set_value("expected_total_weight", total_weightage);
+  frm.set_value("expected_total_amount", total_amount);
+  frm.set_value("expected_making_charge", total_making_charge);
+};
--- aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.py
+++ aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.py
@@ -7,20 +7,6 @@ from frappe.model.document import Document
 
 
 class CustomerJewelleryOrder(Document):
-    def validate(self):
-        if (
-            self.total_expected_weight_per_quantity
-            != self.customer_expected_total_weight
-        ):
-            frappe.throw(
-                _(
-                    "The Sum of Expected Weights Per Quantity must be Equal to Customer Expected Weight"
-                )
-            )
-
-    def on_submit(self):
-        self.create_jewellery_order()
-
 
     def create_jewellery_order(self):
         jewellery_order_exist = frappe.db.exists(
@@ -35,26 +21,25 @@ class CustomerJewelleryOrder(Document):
                 new_jewellery_order.required_date = self.required_date
                 new_jewellery_order.uom = self.uom
                 new_jewellery_order.purity = self.purity
-                new_jewellery_order.customer_expected_total_weight = (
+                new_jewellery_order.expected_total_weight = (
                     item.expected_weight_per_quantity
                 )
                 latest_board_rate = frappe.get_last_doc(
                     "Board Rate", {"purity": self.purity, "uom": "Gram"}
                 )
                 board_rate_value = latest_board_rate.board_rate
-                new_jewellery_order.customer_expected_amount = (
+                new_jewellery_order.expected_total_weight = (
                     item.expected_weight_per_quantity * latest_board_rate.board_rate
                 )
                 new_jewellery_order.uom = self.uom
                 new_jewellery_order.purity = self.purity
                 new_jewellery_order.category = item.item_category
                 new_jewellery_order.type = item.item_type
-                new_jewellery_order.quantity = item.item_quantity
-                new_jewellery_order.status = self.status
+                new_jewellery_order.quantity = item.qty
                 new_jewellery_order.expected_weight_per_quantity = (
                     item.expected_weight_per_quantity
                 )
-                new_jewellery_order.design_attachment = item.item_design_attachment
+                new_jewellery_order.design_attachment = item.design
                 new_jewellery_order.insert(ignore_permissions=True)
                 jewellery_order_count += 1
             frappe.msgprint(
@@ -64,19 +49,3 @@ class CustomerJewelleryOrder(Document):
             )
         else:
             frappe.throw(_(f"Jewellery Order is already exist for {self.name}"))
-
-    @frappe.whitelist()
-    def calculate_customer_expected_amount(self):
-        exists = frappe.db.exists("Board Rate", {"purity": self.purity, "uom": "Gram"})
-        if exists:
-            latest_board_rate = frappe.get_last_doc(
-                "Board Rate", {"purity": self.purity, "uom": "Gram"}
-            )
-            board_rate_value = latest_board_rate.board_rate
-            self.customer_expected_amount = (
-                self.customer_expected_total_weight * board_rate_value
-            )
-        else:
-            frappe.throw(
-                f"No Board Rate found for Purity {self.purity} and UOM {self.uom}"
-            )
