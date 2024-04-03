4e0a79b0f549e0937db24e3a1a70236ca2a4a2df
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2024-04-03 09:26:33 +0530

    Added workflow to Customer Jewellery Order

--- aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.py
+++ aumms/aumms_manufacturing/doctype/customer_jewellery_order/customer_jewellery_order.py
@@ -7,16 +7,6 @@ from frappe.model.document import Document
 
 
 class CustomerJewelleryOrder(Document):
-    # def validate(self):
-    #     if (
-    #         self.total_expected_weight_per_quantity
-    #         != self.customer_expected_total_weight
-    #     ):
-    #         frappe.throw(
-    #             _(
-    #                 "The Sum of Expected Weights Per Quantity must be Equal to Customer Expected Weight"
-    #             )
-    #         )
 
     def on_submit(self):
         self.create_jewellery_order()
@@ -31,7 +21,7 @@ class CustomerJewelleryOrder(Document):
                 new_jewellery_order = frappe.new_doc("Jewellery Order")
                 new_jewellery_order.order_from = "Customer Jewellery Order"
                 new_jewellery_order.customer_jewellery_order = self.name
-                new_jewellery_order.order_item = f"{item.item_category}-{item.item_type}-{item.qty}"
+                # new_jewellery_order.order_item = f"{item.item_category}-{item.item_type}-{item.qty}"
                 new_jewellery_order.customer = self.customer
                 new_jewellery_order.required_date = self.required_date
                 new_jewellery_order.expected_total_weight = (
