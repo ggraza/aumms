3f5218bfcd6d1f7bcc23d1697a325c3d9d441532
Geethu K <geethuk626@gmail.com>
2024-04-16 14:48:17 +0530

    feat:Add intro to jewellery order while validating expected total weight

--- aumms/aumms_manufacturing/doctype/jewellery_order/jewellery_order.js
+++ aumms/aumms_manufacturing/doctype/jewellery_order/jewellery_order.js
@@ -2,56 +2,81 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Jewellery Order", {
+    setup: function(frm) {
+        update_available_item_quantity(frm);
+    },
 
-	setup :function(frm) {
-		update_available_item_quantity(frm);
-	},
-
-	refresh: function(frm) {
-		let total_qty = frm.doc.jewellery_order_items.filter(function(row) {
-			return row.is_available == 1;
-		}).length;
-		frm.set_value('available_item_quantity', total_qty);
-		frm.set_query('uom', () => {
-			return {
-				filters: {
-					"is_purity_uom": 1
-				}
-			};
-		});
-		frm.set_query("item", "jewellery_order_items", ()=> {
-			return {
-				filters: {
-					"item_type": frm.doc.type,
-					"item_category": frm.doc.category
-				}
-			}
-		});
-  	},
-		quantity: function(frm) {
-			limit_item_details(frm)
-		}
+    refresh: function(frm) {
+        let total_qty = 0;
+        if (frm.doc.jewellery_order_items) {
+            total_qty = frm.doc.jewellery_order_items.filter(function(row) {
+                return row.is_available == 1;
+            }).length;
+        }
+        frm.set_value('available_item_quantity', total_qty);
+        frm.set_query('uom', () => {
+            return {
+                filters: {
+                    "is_purity_uom": 1
+                }
+            };
+        });
+        frm.set_query("item", "jewellery_order_items", () => {
+            return {
+                filters: {
+                    "item_type": frm.doc.type,
+                    "item_category": frm.doc.category
+                }
+            }
+        });
+    },
+    quantity: function(frm) {
+        limit_item_details(frm)
+    },
+    total_weight: function(frm) {
+			update_intro(frm)
+    },
+		// on_save: function(frm) {
+		// 	update_intro(frm)
+		// }
 });
+
 
 frappe.ui.form.on("Jewellery Order Item", {
     weight: function(frm, cdt, cdn) {
         let total_weightage = 0;
-        frm.doc.jewellery_order_items.forEach(function(d) {
-            if (d.is_available) {
-                total_weightage += d.weight || 0;
-            }
-        });
+				if(frm.doc.jewellery_order_items){
+					frm.doc.jewellery_order_items.forEach(function(d) {
+	            if (d.is_available) {
+	                total_weightage += d.weight || 0;
+	            }
+	        });
+				}
         frm.set_value('weight_of_available_item', total_weightage);
+				if(frm.doc.jewellery_order_items){
+					frm.doc.jewellery_order_items.forEach(function(d) {
+							total_weightage += d.weight || 0;
+	        });
+				}
+        frm.set_value('total_weight', total_weightage);
     },
 
     jewellery_order_items_remove: function(frm) {
         let total_weightage = 0;
-        frm.doc.jewellery_order_items.forEach(function(d) {
-            if (d.is_available) {
-                total_weightage += d.weight || 0;
-            }
-        });
+				if(frm.doc.jewellery_order_items){
+					frm.doc.jewellery_order_items.forEach(function(d) {
+	            if (d.is_available) {
+	                total_weightage += d.weight || 0;
+	            }
+	        });
+				}
         frm.set_value('weight_of_available_item', total_weightage);
+				if(frm.doc.jewellery_order_items){
+					frm.doc.jewellery_order_items.forEach(function(d) {
+							total_weightage += d.weight || 0;
+	        });
+				}
+        frm.set_value('total_weight', total_weightage);
         update_available_item_quantity(frm);
 				check_finished(frm);
     },
@@ -65,7 +90,7 @@ frappe.ui.form.on("Jewellery Order Item", {
     is_available: function(frm, cdt, cdn) {
         let all_finished = true;
         let total_weightage = 0;
-        let childtable = frm.doc.jewellery_order_items;
+        let childtable = frm.doc.jewellery_order_items || [];
         for (let i = 0; i < childtable.length; i++) {
             if (childtable[i].is_available) {
                 total_weightage += childtable[i].weight || 0;
@@ -85,10 +110,25 @@ frappe.ui.form.on("Jewellery Order Item", {
     }
 });
 
+function update_intro(frm) {
+	if (frm.doc.total_weight != frm.doc.expected_total_weight && !frm.doc.intro_set) {
+			if (!frm.doc.description) {
+					frm.set_intro('Expected Total weight must be equal to the sum of weight in Jewellery Order items', 'blue');
+					frm.doc.intro_set = true;
+			}
+	} else if (frm.doc.total_weight == frm.doc.expected_total_weight && frm.doc.intro_set) {
+			frm.set_intro('');
+			frm.doc.intro_set = false;
+	}
+}
+
 function update_available_item_quantity(frm) {
-    let total_qty = frm.doc.jewellery_order_items.filter(function(row) {
-        return row.is_available == 1;
-    }).length;
+	let total_qty = 0;
+	if(frm.doc.jewellery_order_items){
+		total_qty = frm.doc.jewellery_order_items.filter(function(row) {
+			return row.is_available == 1;
+		}).length;
+	}
     frm.set_df_property('available_item_quantity', 'hidden', total_qty === 0);
     frm.set_value('available_item_quantity', total_qty);
 }
