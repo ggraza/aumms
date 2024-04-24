6c8d426f22d7fb4985f7471380ee50dbb79f0f5b
Geethu K <geethuk626@gmail.com>
2024-04-24 12:33:18 +0530

    feat:status updation in jewellery job card

--- aumms/aumms_manufacturing/doctype/jewellery_job_card/jewellery_job_card.js
+++ aumms/aumms_manufacturing/doctype/jewellery_job_card/jewellery_job_card.js
@@ -1,8 +1,38 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Jewellery Job Card", {
-// 	refresh(frm) {
+frappe.ui.form.on("Jewellery Job Card", {
+  refresh: function(frm){
+    create_custom_buttons(frm);
+  },
+});
 
-// 	},
-// });
+let create_custom_buttons = function(frm){
+  if (frm.doc.status == "Open") {
+    frm.add_custom_button('Start', () => {
+      frm.set_value("status", "Processing");
+      frm.save();
+      frm.page.set_primary_action(__("Pause"), () => {
+        create_custom_buttons(frm);
+      }, "btn-primary");
+    }).addClass("btn-primary");
+  } else if (frm.doc.status == "Processing") {
+    frm.add_custom_button(__("Pause"), () => {
+      frm.set_value("status", "Hold");
+      frm.save();
+      create_custom_buttons(frm);
+    }).addClass("btn-primary");
+  } else if (frm.doc.status == "Hold") {
+    frm.add_custom_button('Start', () => {
+      frm.set_value("status", "Processing");
+      frm.save();
+      create_custom_buttons(frm);
+    }).addClass("btn-primary");
+
+    frm.add_custom_button(__("Done"), () => {
+      frm.set_value("status", "Complete");
+      frm.save();
+      create_custom_buttons(frm);
+    }).addClass("btn-primary");
+  }
+}
